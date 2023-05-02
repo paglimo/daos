3232b900a82519c9a79014f506b3fa6461a3af88
Michael MacDonald <mjmac.macdonald@intel.com>
2023-05-02 18:11:49 +0000

    DAOS-13266 control: Serialize PoolCreate requests
    Adds a new gRPC request serialization mechanism to handle
    PoolCreate requests. Mitigates against a number of edge
    cases that may occur during multiple concurrent PoolCreate
    requests.
    
    Moves non-leader async management out of leadership callback.
    
    Features: pool
    
    Required-githooks: true
    
    Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

--- src/control/server/server.go
+++ src/control/server/server.go
@@ -420,7 +420,7 @@ func (srv *server) registerEvents() {
 	srv.sysdb.OnLeadershipGained(
 		func(ctx context.Context) error {
 			srv.log.Infof("MS leader running on %s", srv.hostname)
-			srv.mgmtSvc.startAsyncLoops(ctx)
+			srv.mgmtSvc.startLeaderLoops(ctx)
 			registerLeaderSubscriptions(srv)
 			srv.log.Debugf("requesting immediate GroupUpdate after leader change")
 			go func() {
@@ -496,6 +496,7 @@ func (srv *server) start(ctx context.Context, shutdown context.CancelFunc) error
 		}
 	}()
 
+	srv.mgmtSvc.startAsyncLoops(ctx)
 	return errors.Wrapf(srv.harness.Start(ctx, srv.sysdb, srv.cfg),
 		"%s harness exited", build.ControlPlaneName)
 }
